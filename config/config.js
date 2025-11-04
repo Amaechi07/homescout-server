48fee3d5f6bdbd32ea4d08674d767366abef67a7
Amaechi07 <amaechiemmanuel017@gmail.com>
2025-11-03 17:29:12 -0800

    config

---
+++ config/config.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  },
+};
