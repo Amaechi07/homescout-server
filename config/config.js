d378a80f9823f4a62d509bc233e72c53609be891
Amaechi07 <amaechiemmanuel017@gmail.com>
2025-11-03 18:14:56 -0800

    config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-export default {
+module.exports = {
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
@@ -10,12 +10,17 @@ export default {
     dialect: "mysql",
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT || 3306,
     dialect: "mysql",
-    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
